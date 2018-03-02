f734ed6711c088e5cbe27b0985b16490c9434c0b
Dardan <lintis008@gmail.com>
2018-03-02 10:22:03 -0500

    Created skeleton folder structure. Added example DB for products and simple navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./../css/navbar.css"
+
+const NavBar = () => {
+    return(
+        <div className="navbar">
+            <nav>
+                <a className="navheader" href="">Home</a> |
+                <a className="navheader" href="">Place Holder</a> |
+                <a className="navheader" href="">Place Holder</a> |
+                <a className="navheader" href="">Place Holder</a>
+            </nav>
+        </div>
+    );
+}
+
+export default NavBar;
---
+++ src/scenes/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import NavBar from './../components/NavBar'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <NavBar/>
+      </div>
+    );
+  }
+}
+
+export default Home;
